82c9a03dae55a73143940d752684a82256f8781a
lolabird <octavia@octavianichols.com>
2023-10-15 18:11:45 -0600

    Chapter 8 correction for problem 3

--- thinkcspy/Chapter-8-Exercises/Python-files/Problem3.py
+++ thinkcspy/Chapter-8-Exercises/Python-files/Problem3.py
@@ -2,23 +2,12 @@
 # Chapter 8
 # Problem 3
 
-import math
-
 def is_prime(n):
-    # your code here
-    
-    x = 3
-  
-    if abs(n) > 2:
-        while x < math.sqrt(n):
-            if n % 2 == 0:
-                return False
-            elif n % x == 0:
-                return False
-            else:
-                x += 2
-        return True
-    else:
-        return True
+    # your code 
+        
+    for i in range (2, n):
+        if n % i == 0:
+            return False
+    return True
 
-print(is_prime(4813))
+print(is_prime(25))
