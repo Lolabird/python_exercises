8a69d379b95f1a8f11767622c212f917078fc435
lolabird <octavia@octavianichols.com>
2023-11-19 16:24:06 -0700

    Count words in file

--- thinkcspy/Chapter-12-Exercises/Problem3.py
+++ thinkcspy/Chapter-12-Exercises/Problem3.py
@@ -1,1 +1,24 @@
-open(alice_in_wonderland.txt, 'r')
+aliceBook = open("thinkcspy/Chapter-12-Exercises/alice_in_wonderland.txt", "r")
+aliceDict = {}
+
+for line in aliceBook:
+    words = line.split()
+
+    for word in words:
+        word = word.lower()
+        newWord = ""
+
+        for char in word:
+            if char.isalpha():
+                newWord += char
+
+        word = newWord            
+
+        if word not in aliceDict:
+            aliceDict[word] = 1
+        else:
+            aliceDict[word] += 1
+
+print(aliceDict)
+
+aliceBook.close()
