55a6b0c282ff6cbf80414364d08e5a570df81048
lolabird <octavia@octavianichols.com>
2024-02-16 16:56:28 -0700

    Add initial methods

--- thinkcspy/Chapter-17-18-Exercises/rectangle.py
+++ thinkcspy/Chapter-17-18-Exercises/rectangle.py
@@ -1,4 +1,21 @@
 from points import Point
 
-a = Point(5, 6)
-print(a)
+class Rectangle:
+    def __init__(self, pos, width, height):
+        self.pos = pos
+        self.width = width
+        self.height = height
+
+    def getWidth(self):
+        return self.width
+    
+    def getHeight(self):
+        return self.height
+    
+    def __str__(self):
+        return "Lower Left Coordinates: {}; Width: {}; Height: {}".format(self.pos, self.width, self.height)
+    
+
+r = Rectangle(Point(4, 5), 6, 5)
+print(r)
+    
